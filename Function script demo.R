fa7984f5218436b230111d78175d54300de2ed68
Alexandra Reich <agreich@alaska.edu>
2022-04-18 12:15:04 -0800

    Ready to record!!

--- Function script demo.R
+++ Function script demo.R
@@ -30,15 +30,33 @@ mean(fish_length) #check to make sure my function works
 
 #Let's try something more useful
 ##von Bertalaffy length at age equation:
-####
+####find predicted fish lengths given fish ages, max growth, ect.
 
 von_Bert <- function(age, Linf, k, t0) {
   pred.length <- Linf*(1-exp(-k*(age-t0))) #the von Bertalaffy equation
   return(pred.length)
 }
 
-#a real dataset!
-goa <- read.csv("pollock_race_specimen copy.csv", header=TRUE, skip=6)
+
+#say I have a pollock dataset with these parameters:
+p_Linf <- 700 #max asymptotic length
+p_k <- 0.2 #growth coefficient
+p_t0 <- 0  #age of fish when fish is length 0
+p_age <- runif(30, 0, 17) #simulating fish age vector
+
+##CODE HERE
+
+
+#you can easily use this same calcualtion with other data!
+##say we want just the male pollock data:
+##where Linf is 600 instead of 700
+p_male_Linf <- 600
+
+
+##CODE HERE
+
+  
+  
 
 
 
