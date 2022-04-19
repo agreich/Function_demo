d079f34d1afcf6998c45e92b6220d47a09d18fe1
Alexandra Reich <agreich@alaska.edu>
2022-04-18 16:50:45 -0800

    Done with video, pretty much. Need to preview and upload

--- Function script demo.R
+++ Function script demo.R
@@ -13,7 +13,7 @@
 #Body of function... what you want the function to do!
 #}
 
-
+################################################################
 #Example: create a function that calculates the sum
 ##example:
 my_mean <- function(vector){   #NAME <- function(ARGUMENT)
@@ -27,7 +27,7 @@ my_mean(fish_length)
 mean(fish_length) #check to make sure my function works
 
 
-
+##########################################################################
 #Let's try something more useful
 ##von Bertalaffy length at age equation:
 ####find predicted fish lengths given fish ages, max growth, ect.
@@ -44,13 +44,16 @@ p_k <- 0.2 #growth coefficient
 p_t0 <- 0  #age of fish when fish is length 0
 p_age <- runif(30, 0, 17) #simulating fish age vector
 
-##CODE HERE
+p_predicted_L <- von_Bert(age=p_age, Linf=p_Linf, k=p_k, t0=p_t0)
+p_predicted_L
 
 
 #you can easily use this same calcualtion with other data!
 ##say we want just the male pollock data:
 ##where Linf is 600 instead of 700
 p_male_Linf <- 600
+
+p_male_predicted_L <- von_Bert(age=p_age, Linf=p_male_Linf, k=p_k, t0=p_t0)
 
 
 ##CODE HERE
